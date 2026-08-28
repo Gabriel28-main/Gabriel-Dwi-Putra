51c4e15379bd12259e0e7d1be264ebc48a532dd6
Gabriel <gab3ng67@gmail.com>
2026-08-28 23:24:12 +0800

    feat: adding meta data for the title tab

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Home from './component/Home'
 import Navbar from './component/Navbar'
 import About from './component/about'
+import { Metadata } from 'next'
+
+export const metadata = {
+  title: 'Gabriel Dwi Putra',
+};
 
 export default function page() {
   return (
