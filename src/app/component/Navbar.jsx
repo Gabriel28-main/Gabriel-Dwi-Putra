a7045fa356a2d88318597e0e07d65d065fbefa3f
Gabriel <gab3ng67@gmail.com>
2026-08-28 23:19:10 +0800

    fix: adjust the color of the navbar & bold the navbar button

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div className='w-screen fixed h-20 items-center flex gap-[750px] justify-center'>
+        <div className='w-screen fixed h-20 items-center flex gap-[750px] justify-center bg-[#131218]'>
             <div className='items-center'>
                 <h3 className='font-extrabold text-white text-xl'>Gabriel Dwi Putra</h3>
             </div>
             <div className='text-white flex gap-25 items-center'>
-                <a href="">Home</a>
-                <a href="">About Me</a>
-                <a href="">Projects</a>
-                <a href="">Contact</a>
+                <a href="" className='hover:text-[#FFD401] font-bold'>Home</a>
+                <a href="" className='hover:text-[#FFD401] font-bold'>About Me</a>
+                <a href="" className='hover:text-[#FFD401] font-bold'>Projects</a>
+                <a href="" className='hover:text-[#FFD401] font-bold'>Contact</a>
             </div>
         </div>
     )
