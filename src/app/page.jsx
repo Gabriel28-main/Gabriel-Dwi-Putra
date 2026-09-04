f47d88374b9d01679b8343c5a41badaae9fe02ae
Gabriel <gab3ng67@gmail.com>
2026-09-04 15:01:10 +0800

    restricted: unable the navbar and about for a while. need to fixing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import Home from './component/Home'
-import Navbar from './component/Navbar'
-import About from './component/about'
-import { Metadata } from 'next'
-
-export const metadata = {
-  title: 'Gabriel Dwi Putra',
-};
-
+// import Navbar from './component/Navbar'
+// import About from './component/about'
 export default function page() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Home />
-      <About />
+      {/* <About /> */}
     </div>
   )
 }
